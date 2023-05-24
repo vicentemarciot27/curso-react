b215e6cee4122b5120778572c528222c0dffada2
vicentemarciot27 <vicentemarciot27@gmail.com>
2023-05-23 21:38:23 -0300

    added new components

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import alunos from "../../data/alunos";
 
 export default props =>{
+    const min = 50
+    const max = 70
+    const gerarIdade = () => parseInt(Math.random() * (max-min)) + min
+    const nomes = alunos.map(aluno => aluno.nome)
+    const gerarNome = () => nomes[parseInt(Math.random() * nomes.length)]
+    const gerarNerd = () => Math.random() > 0.5
     return(
         <div>
             <div>
@@ -9,7 +16,7 @@ export default props =>{
             </div>
             <button onClick={
                 function (e) {
-                    props.quandoClicar('João, 53, true')
+                    props.quandoClicar(gerarNome(), gerarIdade(), gerarNerd())
                 }
             }>Fornecer Informações</button>
         </div>
